27c66259409d1008d7590f14882a7ba01b3061ec
Sid Ahmed Sahraoui <s.sahraoui@esi-sba.dz>
2023-05-04 17:42:27 +0000

    add More Pages

--- src/components/interview/style.tsx
+++ src/components/interview/style.tsx
@@ -3,23 +3,9 @@ import { createUseStyles } from "react-jss";
 const useStyles = createUseStyles({
   main: {
     width: "80%",
-    margin: "30px auto",
+    margin: "10px auto",
     "@media screen and (max-width: 1268px)": {
       width: "90%",
-    },
-    "& nav": {
-      display: "flex",
-      justifyContent: "space-between",
-      "& h1": {
-        fontSize: "24px",
-        fontWeight: 400,
-        fontFamily: "Rubik",
-      },
-      "& .icons": {
-        width: "80px",
-        display: "flex",
-        justifyContent: "space-between",
-      },
     },
   }})
 
--- src/components/sharing/index.tsx
+++ src/components/sharing/index.tsx
@@ -13,7 +13,6 @@ import Full from '../../../public/img/fullstack.png'
 import Canada from '../../../public/img/canada.png'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendarCheck } from "@fortawesome/free-solid-svg-icons";
-import { ChevronRightIcon } from "@heroicons/react/20/solid";
 
 
 const defaultCategories = {
@@ -27,16 +26,16 @@ const defaultCategories = {
           space: 1
         },
         {
-          id: 2,
+          id: 1,
           title: 'Fullstack Developer',
           description: 'Assurez-vous d\'avoir les connaissances nécessaires pour obtenir ' +
           'votre premier emploi en tant qu\'ingénieur Fullstack web et grandir dans ' +
           'votre première étape dans le domaine informatique',
           image: Full,
-          space: 1
+          space: 5
         },
         {
-          id: 3,
+          id: 1,
           title: 'UI/UX Designer',
           description: 'Passez un peu de temps à ' +
           'préparer votre prochain entretien avec un concepteur d\'expérience utilisateur (UX) ' +
@@ -44,39 +43,24 @@ const defaultCategories = {
           'd\'un entretien consiste à passer en revue certaines questions d\'entretien courantes' + 
           ' et à réfléchir à la manière dont vous y répondrez.',
           image: UI,
-          space: 1
+          space: 5
         },
         {
-          id: 4,
+          id: 1,
           title: 'Backend Developer',
           description: 'Pratiquer toutes les questions de compétences backend,' 
           +'de la conception de l\'API à la mise en œuvre et aux meilleures pratiques',
           image: Back,
-          space: 1
+          space: 5
         },
         {
-          id: 5,
+          id: 1,
           title: 'Frontend Developer',
           description: 'Assurez-vous d\'avoir les connaissances nécessaires pour obtenir ' +
           'votre premier emploi en tant qu\'ingénieur frontend et grandir dans ' +
           'votre première étape dans le domaine informatique',
           image: Front,
-          space: 1
-        },
-        {
-          id: 6,
-          title: 'Entretien Etudier a Canada',
-          description: 'Pratiquez l\'entretien Etudier a Canada avec nos experts n\'hésitez plus ! '+
-          'réservez maintenant un entretien 1: 1 et augmentez vos chances d\'être accepté,',
-          image: Canada,
-          space: 1
-        },
-        {
-          id: 7,
-          title: 'HR Interviews Preparation',
-          description: 'Préparation aux entretiens RH avec nos experts n\'hésitez plus ! reservez maintenant un entretien 1: 1 et augmentez vos chances de trouver un emploi.',
-          image: Inter,
-          space: 1
+          space: 5
         },
       ],
     "Études et Démarche": [
@@ -89,7 +73,7 @@ const defaultCategories = {
         space: 1
       },
       {
-        id: 6,
+        id: 1,
         title: 'Entretien Etudier a Canada',
         description: 'Pratiquez l\'entretien Etudier a Canada avec nos experts n\'hésitez plus ! '+
         'réservez maintenant un entretien 1: 1 et augmentez vos chances d\'être accepté,',
@@ -100,16 +84,16 @@ const defaultCategories = {
     
     'Technical Interviews': [
       {
-        id: 2,
+        id: 1,
         title: 'Fullstack Developer',
         description: 'Assurez-vous d\'avoir les connaissances nécessaires pour obtenir ' +
         'votre premier emploi en tant qu\'ingénieur Fullstack web et grandir dans ' +
         'votre première étape dans le domaine informatique',
         image: Full,
-        space: 1
+        space: 5
       },
       {
-        id: 3,
+        id: 1,
         title: 'UI/UX Designer',
         description: 'Passez un peu de temps à ' +
         'préparer votre prochain entretien avec un concepteur d\'expérience utilisateur (UX) ' +
@@ -117,33 +101,33 @@ const defaultCategories = {
         'd\'un entretien consiste à passer en revue certaines questions d\'entretien courantes' + 
         ' et à réfléchir à la manière dont vous y répondrez.',
         image: UI,
-        space: 1
+        space: 5
       },
       {
-        id: 4,
+        id: 1,
         title: 'Backend Developer',
         description: 'Pratiquer toutes les questions de compétences backend,' 
         +'de la conception de l\'API à la mise en œuvre et aux meilleures pratiques',
         image: Back,
-        space: 1
+        space: 5
       },
       {
-        id: 5,
+        id: 1,
         title: 'Frontend Developer',
         description: 'Assurez-vous d\'avoir les connaissances nécessaires pour obtenir ' +
         'votre premier emploi en tant qu\'ingénieur frontend et grandir dans ' +
         'votre première étape dans le domaine informatique',
         image: Front,
-        space: 1
+        space: 5
       },
     ],
     "HR Interviews": [
         {
-          id: 7,
-          title: 'HR Interviews Preparation',
-          description: 'Préparation aux entretiens RH avec nos experts n\'hésitez plus ! reservez maintenant un entretien 1: 1 et augmentez vos chances de trouver un emploi.',
+          id: 1,
+          title: 'Programme Campus France',
+          description: 'Préparation aux entretiens de Campus France',
           image: Inter,
-          space: 1
+          space: 5
         },
         
       ],
@@ -158,7 +142,7 @@ const Index: React.FC<Categories> = ({ categories }) => {
   return (
     <>
       <main className={classes.main}>
-      <div className="w-full max-w-6xl m-auto px-2 py-4 sm:px-0">
+      <div className="w-full max-w-6xl m-auto px-2 py-16 sm:px-0">
       <Tab.Group>
         <Tab.List className="flex space-x-1 rounded-xl bg-none p-1">
           {Object.keys(defaultCategories).map((category) => (
@@ -167,9 +151,9 @@ const Index: React.FC<Categories> = ({ categories }) => {
               className={({ selected }) =>
                 classNames(
                   'w-full rounded-lg py-4 px-4 text-md font-medium leading-5 text-gray-one',
-                  'focus:outline-none',
+                  'focus:outline-none hover:text-gray-one',
                   selected
-                    ? 'bg-gray-800 text-white'
+                    ? 'bg-purple-two'
                     : 'text-gray-one'
                 )
               }
@@ -211,15 +195,9 @@ const Index: React.FC<Categories> = ({ categories }) => {
                       <h6 className="text-gray-400 text-sm font-normal pt-2  mb-4 max-w-xl leading-5">
                         {post.description}
                       </h6>
-                      <Link href='/' className="flex cursor-pointer flex-row items-center max-w-fit h-auto px-4 py-2 font-semibold
-                       transition duration-200 border ease-in-out transform rounded-md outline-none mt-7 group 
-                       text-gray-100 hover:text-gray-700 hover:border-gray-500 hover:bg-gray-50 focus:outline-none bg-darkblue 
-                       hover:text-lightpink hover:border-darkpink text-darkpink border-lightblue">
-                      
-                                        {'Plus d\'infos'}
-                              <ChevronRightIcon className="w-5 h-5 ml-2" aria-hidden="true" />
+                      <Link href='/' className='px-4 py-2 mt-4 text-white rounded-md border-2' >
+                              {'Plus d\'infos'}
                       </Link>
-                      
                     </div>  
                     <Link href='/' className="p-4 w-fit flex flex-col justify-start bg-bg-green rounded-r-xl" >
                           <h4  className='h-fit w-full text-white text-lg font-bold text-center' >
